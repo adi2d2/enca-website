b6f826b1d2d657c7c262180fb4e7d3bb524c1f3a
Alex Radulescu <radulescu.alexandru.doru@gmail.com>
2022-10-24 19:38:16 +0800

    Updated LinkedIn Icon

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -21,18 +21,18 @@ export const Footer = () => {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
+              width="32"
+              height="32"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
+              stroke-width="0.5"
               stroke-linecap="round"
               stroke-linejoin="round"
             >
-              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-              <rect x="2" y="9" width="4" height="12"></rect>
-              <circle cx="4" cy="4" r="2"></circle>
+              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+              <rect x="2" y="9" width="4" height="12" />
+              <circle cx="4" cy="4" r="2" />
             </svg>
           </SoMeLink>
         </HStack>
