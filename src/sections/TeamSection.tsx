1e63b82fc6f114fb890073347402da41d4ac4c66
adi2d2 <131524976+adi2d2@users.noreply.github.com>
2023-04-22 19:42:57 +0530

    Update TeamSection.tsx

--- src/sections/TeamSection.tsx
+++ src/sections/TeamSection.tsx
@@ -8,7 +8,7 @@ import adithyaAvatarUrl from '../assets/adithya.png'
 export const TeamSection = () => {
   return (
     <StyledSection id="team">
-      <StyledVStack gap="9">
+      <StyledVStack gap="50">
         <Heading size="7" align="center" weight="thin">
           Meet <strong>Our Team</strong>
         </Heading>
